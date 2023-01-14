7551aa309d9820b6e88fff063d9ec6d1f873e9a8
ariaonart <ariaonart@gmail.com>
2023-01-15 00:01:14 +0900

    2753

---
+++ 2753/2753.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // 4의 배수이면서, 
+    // (n % 4)
+    if (n % 4 != 0) {
+        cout << 0;
+        return 0;
+    }
+
+    if (n % 400 == 0) {
+        cout << 1;
+        return 0;
+    }
+
+    if (n % 100 == 0) {
+        cout << 0;
+        return 0;
+    }
+
+    cout << 1;
+
+    return 0;
+}
