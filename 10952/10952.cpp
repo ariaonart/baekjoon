c9a679d0fe2c2e4e1478dc9d028150c1e9a6134c
ariaonart <ariaonart@gmail.com>
2023-02-05 17:37:23 +0900

    10039,10052

---
+++ 10952/10952.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	while (true) {
+		int a, b;
+		cin >> a >> b;
+
+		if (a == 0 && b == 0) {
+			break;
+		}
+		cout << a + b;
+	}
+
+	return 0;
+}
+
